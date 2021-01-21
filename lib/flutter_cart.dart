7c08372bf4fe7e18920977ba8bab63e548eb5d60
Osama Asif <osama.asif20@gmail.com>
2021-01-21 16:23:08 +0500

    product price values fixed

--- lib/flutter_cart.dart
+++ lib/flutter_cart.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 
 import 'model/cart_model.dart';
@@ -7,45 +5,35 @@ import 'model/cart_model.dart';
 class FlutterCart {
   static final FlutterCart _instance = FlutterCart._internal();
   CartItem _cartItem;
+  // CartItemElement _cartItemElement;
   List<CartItem> _cartItemList;
   List<CartItem> get cartItem => _cartItemList;
   List<String> _uuid;
   bool _filterItemFound = false;
   var message;
-
   factory FlutterCart() {
     return _instance;
   }
-
   FlutterCart._internal() {
     _cartItemList = new List<CartItem>();
     _uuid = List<String>();
   }
 
-  /// This method is called when we have to add [product] into cart
+  /// This method is called when we have to add [productTemp] into cart
   addToCart(
       {@required dynamic productId,
       @required dynamic unitPrice,
       @required int quantity,
-
-      ///[uniqueCheck] is used to differentiate the type between item
-      ///[e.g] the shirt sizes in (LARGE, MEDIUM, SMALL) the [Product ID] will remain same
-      ///But if UUID is not present so, how we can differentiate between them? So in this case we will
-      ///User the uniqueCheck
       dynamic uniqueCheck,
-
-      ///[productDetailsObject] is used as a dump variable you can dump your object and any kind of data
-      ///that you wanted use in future.
       dynamic productDetailsObject}) {
     _cartItem = new CartItem();
     _setProductValues(
         productId, unitPrice, quantity, uniqueCheck, productDetailsObject);
-
     if (_cartItemList.isEmpty) {
-      _cartItem.subTotal = unitPrice * quantity;
+      _cartItem.subTotal =
+          double.parse((quantity * unitPrice).toStringAsFixed(2));
       _uuid.add(_cartItem.uuid);
       _cartItemList.add(_cartItem);
-      print(json.encode(_cartItem));
       return {"status": true, "message": _successMessage};
     } else {
       _cartItemList.forEach((x) {
@@ -55,7 +43,6 @@ class FlutterCart {
             if (x.uniqueCheck == uniqueCheck) {
               _filterItemFound = true;
               _updateProductDetails(x, quantity, unitPrice);
-              print(1);
               message = {"status": true, "message": _updateMessage};
             }
           }
@@ -74,7 +61,7 @@ class FlutterCart {
         _uuid.add(_cartItem.uuid);
         _updateProductDetails(_cartItem, quantity, unitPrice);
         _cartItemList.add(_cartItem);
-        print(2);
+
         message = {"status": true, "message": _successMessage};
       }
       _filterItemFound = false;
@@ -87,18 +74,41 @@ class FlutterCart {
     if (_cartItemList[index].quantity > 1) {
       _cartItemList[index].quantity = --_cartItemList[index].quantity;
       _cartItemList[index].subTotal =
-          _cartItemList[index].quantity * _cartItemList[index].unitPrice;
+          (_cartItemList[index].quantity * _cartItemList[index].unitPrice)
+              .roundToDouble();
     } else {
       _cartItemList.removeAt(index);
       return {"status": true, "message": _removedMessage};
+      // return true;
     }
+  }
+
+  deleteItemFromCart(int index, {int specificIndex}) {
+    for (int i = _cartItemList[index].quantity; i > 0; i--) {
+      decrementItemFromCart(index);
+    }
+  }
+
+  deleteAllCart() {
+    _cartItemList = new List<CartItem>();
+    _uuid = List<String>();
+  }
+
+  int findItemIndexFromCart(cartId) {
+    for (int i = 0; i < _cartItemList.length; i++) {
+      if (_cartItemList[i].productId == cartId) {
+        return i;
+      }
+    }
+    return null;
   }
 
   /// This function is used to increment the item quantity into cart
   incrementItemToCart(int index) {
     _cartItemList[index].quantity = ++_cartItemList[index].quantity;
     _cartItemList[index].subTotal =
-        _cartItemList[index].quantity * _cartItemList[index].unitPrice;
+        (_cartItemList[index].quantity * _cartItemList[index].unitPrice)
+            .roundToDouble();
   }
 
   /// This method is called when we have to [initialize the values in our cart object]
@@ -116,45 +126,21 @@ class FlutterCart {
   /// This method is called when we have to update the [product details in  our cart]
   void _updateProductDetails(cartObject, int quantity, dynamic unitPrice) {
     cartObject.quantity = quantity;
-    cartObject.subTotal = quantity * unitPrice;
+    cartObject.subTotal =
+        double.parse((quantity * unitPrice).toStringAsFixed(2));
   }
 
   /// This method is called when we have to get the [cart lenght]
   getCartItemCount() {
     return _cartItemList.length;
   }
-
-  /// This method is used for getting the total amount.
+ /// This method is called when we have to get the [Total amount]
   getTotalAmount() {
     double totalAmount = 0.0;
     _cartItemList.forEach((e) => totalAmount += e.subTotal);
     return totalAmount;
   }
 
-  //find specific item index in cart
-  int findItemIndexFromCart(currentProductId) {
-    for (int i = 0; i < _cartItemList.length; i++) {
-      if (_cartItemList[i].productId == currentProductId) {
-        return i;
-      }
-    }
-    return null;
-  }
-  
-  //delete specific product item from cart
-  deleteItemFromCart(int index) {
-    for (int i = _cartItemList[index].quantity; i > 0; i--) {
-      decrementItemFromCart(index);
-    }
-  }
-
-  //clear whole cart
-  deleteAllCart() {
-    _cartItemList = new List<CartItem>();
-    _uuid = List<String>();
-  }
-
-  /// static messages
   static final String _successMessage = "Item added to cart successfully.";
   static final String _updateMessage = "Item updated successfully.";
   static final String _removedMessage = "Item removed from cart successfully.";
